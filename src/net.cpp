bdf1408f54a707180a13ec265c467908ceeee072
BitCoin BOB <dobbscoin@gmail.com>
2014-01-20 18:01:33 -0600

    Update net.cpp
    Updated seed nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1191,7 +1191,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"bluematt.me", "coingen-seed-scrypt.bluematt.me"},
+    {"slackprojects.org", "antelope.slackprojects.org"},
     {NULL, NULL}
 };
 
