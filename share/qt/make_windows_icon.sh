e349f92159f4ab5c853a0f8ed2fbdc2d1191ef73
Earlz <earlz@earlz.net>
2015-05-19 22:48:32 -0400

    clear out old code

--- share/qt/make_windows_icon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/dobbscoin.png
-ICON_DST=../../src/qt/res/icons/dobbscoin.ico
-convert ${ICON_SRC} -resize 16x16 dobbscoin-16.png
-convert ${ICON_SRC} -resize 32x32 dobbscoin-32.png
-convert ${ICON_SRC} -resize 48x48 dobbscoin-48.png
-convert dobbscoin-16.png dobbscoin-32.png dobbscoin-48.png ${ICON_DST}
-
