80e8f17d60b8afa533c8c6744e0e0813a7c54a38
Earlz <earlz@earlz.net>
2015-05-20 02:04:52 -0400

    I don't know wtf it is, but BOB's wormhole is yielding different results... for some unknown reason

--- src/pow.cpp
+++ src/pow.cpp
@@ -12,6 +12,7 @@
 #include "uint256.h"
 #include "util.h"
 
+
 unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const CBlockHeader *pblock, uint64_t TargetBlocksSpacingSeconds, uint64_t PastBlocksMin, uint64_t PastBlocksMax) {
     /* current difficulty formula, megacoin - kimoto gravity well */
     const CBlockIndex  *BlockLastSolved             = pindexLast;
@@ -28,7 +29,7 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const CBloc
     double              EventHorizonDeviation;
     double              EventHorizonDeviationFast;
     double              EventHorizonDeviationSlow;
-    const uint256 &bnProofOfWorkLimit = Params().ProofOfWorkLimit();
+    const uint256 bnProofOfWorkLimit = Params().ProofOfWorkLimit();
     
     if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || (uint64_t)BlockLastSolved->nHeight < PastBlocksMin) { return bnProofOfWorkLimit.GetCompact(); }
     
@@ -59,7 +60,8 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const CBloc
         BlockReading = BlockReading->pprev;
     }
     
-    uint256 bnNew(PastDifficultyAverage);
+    uint256 bnNew;
+    bnNew=PastDifficultyAverage;
     if (PastRateActualSeconds != 0 && PastRateTargetSeconds != 0) {
         bnNew *= PastRateActualSeconds;
         bnNew /= PastRateTargetSeconds;
@@ -152,19 +154,113 @@ unsigned int GetNextWorkRequired_V1(const CBlockIndex* pindexLast, const CBlockH
     return bnNew.GetCompact();
 }
 
+
+unsigned int GetNextWorkRequired_V3(const CBlockIndex* pindexLast, const CBlockHeader *pblock)
+{
+
+    static const int64_t nTargetTimespan_V3 = 10*60 ; // dobbscoin: every 10 minutes
+    static const int64_t nTargetSpacing_V3 = 10*60; // dobbscoin: 10 minutes
+    static const int64_t nInterval_V3 = nTargetTimespan_V3 / nTargetSpacing_V3;
+
+    unsigned int nProofOfWorkLimit = Params().ProofOfWorkLimit().GetCompact();
+
+    
+    int64_t retargetTimespan = nTargetTimespan_V3; //Params().TargetTimespan();
+    int64_t retargetInterval = nInterval_V3; //Params().Interval();
+    
+    //retargetInterval = Params().TargetTimespan() / Params().TargetSpacing();
+    //retargetTimespan = Params().TargetTimespan();
+    
+    // Genesis block
+    if (pindexLast == NULL)
+        return nProofOfWorkLimit;
+
+    // Only change once per interval
+    if ((pindexLast->nHeight+1) % retargetInterval != 0)
+    {
+        if (Params().AllowMinDifficultyBlocks())
+        {
+            // Special difficulty rule for testnet:
+            // If the new block's timestamp is more than 2* nTargetSpacing minutes
+            // then allow mining of a min-difficulty block.
+            if (pblock->nTime > pindexLast->nTime + nTargetSpacing_V3 * 30) //Params().TargetSpacing()*30)
+                return nProofOfWorkLimit;
+            else
+            {
+                // Return the last non-special-min-difficulty-rules-block
+                const CBlockIndex* pindex = pindexLast;
+                while (pindex->pprev && pindex->nHeight % retargetInterval != 0 && pindex->nBits == nProofOfWorkLimit)
+                    pindex = pindex->pprev;
+                return pindex->nBits;
+            }
+        }
+        return pindexLast->nBits;
+    }
+
+    // Fractalcoin: This fixes an issue where a 51% attack can change difficulty at will.
+    // Go back the full period unless it's the first retarget after genesis. Code courtesy of Art Forz
+    int blockstogoback = retargetInterval-1;
+    if ((pindexLast->nHeight+1) != retargetInterval)
+        blockstogoback = retargetInterval;
+
+    // Go back by what we want to be 14 days worth of blocks
+    const CBlockIndex* pindexFirst = pindexLast;
+    for (int i = 0; pindexFirst && i < blockstogoback; i++)
+        pindexFirst = pindexFirst->pprev;
+    assert(pindexFirst);
+
+    // Limit adjustment step
+    int64_t nActualTimespan = pindexLast->GetBlockTime() - pindexFirst->GetBlockTime();
+    LogPrintf("  nActualTimespan = %d  before bounds\n", nActualTimespan);
+    
+    //DigiShield implementation - thanks to RealSolid & WDC for this code
+    // amplitude filter - thanks to daft27 for this code
+    nActualTimespan = retargetTimespan + (nActualTimespan - retargetTimespan)/8;
+
+    if (nActualTimespan < (retargetTimespan - (retargetTimespan/4)) ) nActualTimespan = (retargetTimespan - (retargetTimespan/4));
+    if (nActualTimespan > (retargetTimespan + (retargetTimespan/2)) ) nActualTimespan = (retargetTimespan + (retargetTimespan/2));
+
+    // Retarget
+    uint256 bnNew;
+    uint256 bnOld;
+    bnNew.SetCompact(pindexLast->nBits);
+    bnOld = bnNew;
+    //scale up for millisecond granularity
+    bnNew *= nActualTimespan;
+    //slingshield effectively works by making the target block time longer temporarily
+    bnNew /= (retargetTimespan); 
+
+
+
+    if (bnNew > Params().ProofOfWorkLimit())
+        bnNew = Params().ProofOfWorkLimit();
+
+    /// debug print
+    LogPrintf("GetNextWorkRequired DIGISHIELD RETARGET\n");
+    LogPrintf("Params().TargetTimespan() = %d    nActualTimespan = %d\n", Params().TargetTimespan(), nActualTimespan);
+    LogPrintf("Before: %08x  %s\n", pindexLast->nBits, bnOld.ToString());
+    LogPrintf("After:  %08x  %s\n", bnNew.GetCompact(), bnNew.ToString());
+    return bnNew.GetCompact();
+}
+
+
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock)
 {
     int DiffMode = 1;
     if (Params().AllowMinDifficultyBlocks()) {
-    //    if (pindexLast->nHeight+1 >= 50) { DiffMode = 2; }
+        if (pindexLast->nHeight+1 >= 50) { DiffMode = 2; }
+        if (pindexLast->nHeight+1 >= 100) { DiffMode = 3; }
+
     }
-    else {
+    else 
+    {
         if (pindexLast->nHeight+1 >= 13579) { DiffMode = 2; } // KGW @ Block 13579
+        if(pindexLast->nHeight+1 >= 31597) { DiffMode = 3; } //switch to dobbshield @ block 31597
     }
-    
     if      (DiffMode == 1) { return GetNextWorkRequired_V1(pindexLast, pblock); }
     else if (DiffMode == 2) { return GetNextWorkRequired_V2(pindexLast, pblock); }
-    return GetNextWorkRequired_V2(pindexLast, pblock);
+    else if (DiffMode == 3) { return GetNextWorkRequired_V3(pindexLast, pblock); }
+    return GetNextWorkRequired_V3(pindexLast, pblock); //never reached..
 }
 
 
