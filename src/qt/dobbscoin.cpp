12e25eaddee37ab758c472720bb8c48c3a3ccd40
Earlz <earlz@earlz.net>
2015-06-20 12:12:59 -0400

    Restyle to include tiled BOB background

--- src/qt/dobbscoin.cpp
+++ src/qt/dobbscoin.cpp
@@ -528,7 +528,7 @@ int main(int argc, char *argv[])
     QApplication::setOrganizationDomain(QAPP_ORG_DOMAIN);
     QApplication::setApplicationName(QAPP_APP_NAME_DEFAULT);
     GUIUtil::SubstituteFonts(GetLangTerritory());
-
+    qApp->setStyleSheet(" background-image: url(\":res/images/bgtile.png\");");
     /// 4. Initialization of translations, so that intro dialog is in user's language
     // Now that QSettings are accessible, initialize translations
     QTranslator qtTranslatorBase, qtTranslator, translatorBase, translator;
