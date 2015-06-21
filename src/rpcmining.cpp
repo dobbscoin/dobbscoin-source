075d4f70341268f32b9e3271a91f312f606a1722
Earlz <earlz@earlz.net>
2015-06-21 14:01:47 -0400

    disable this check for mining since it seems to cause a lot of problems all the time

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -431,8 +431,8 @@ Value getblocktemplate(const Array& params, bool fHelp)
     if (vNodes.empty())
         throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "Dobbscoin is not connected!");
 
-    if (IsInitialBlockDownload())
-        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Dobbscoin is downloading blocks...");
+   // if (IsInitialBlockDownload())
+   //     throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Dobbscoin is downloading blocks...");
 
     static unsigned int nTransactionsUpdatedLast;
 
