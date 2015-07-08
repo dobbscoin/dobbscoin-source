56981bb40cdfedf7ee62dcd2b4556fa7224bab7b
Earlz <earlz@earlz.net>
2015-07-08 07:52:26 -0400

    Well, tried adding back in getwork but doesn't seem to be working

--- src/init.cpp
+++ src/init.cpp
@@ -183,6 +183,7 @@ void Shutdown()
         delete pblocktree;
         pblocktree = NULL;
     }
+    ShutdownRPCMining();
 #ifdef ENABLE_WALLET
     if (pwalletMain)
         bitdb.Flush(true);
@@ -1281,7 +1282,8 @@ bool AppInit2(boost::thread_group& threadGroup)
 #endif
 
     StartNode(threadGroup);
-
+    // InitRPCMining is needed here so getwork/getblocktemplate in the GUI debug console works properly.
+    InitRPCMining();
 #ifdef ENABLE_WALLET
     // Generate coins in the background
     if (pwalletMain)
--- src/rpcserver.h
+++ src/rpcserver.h
@@ -225,7 +225,7 @@ extern json_spirit::Value verifychain(const json_spirit::Array& params, bool fHe
 extern json_spirit::Value getchaintips(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value invalidateblock(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value reconsiderblock(const json_spirit::Array& params, bool fHelp);
-
+extern json_spirit::Value getwork(const json_spirit::Array& params, bool fHelp);
 // in rest.cpp
 extern bool HTTPReq_REST(AcceptedConnection *conn,
                   std::string& strURI,
