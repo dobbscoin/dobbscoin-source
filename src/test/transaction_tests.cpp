7e845b9cc37c964d644e31147d47c2bc7c092476
Earlz <earlz@earlz.net>
2015-08-31 23:34:19 -0400

    fix bad transaction test

--- src/test/transaction_tests.cpp
+++ src/test/transaction_tests.cpp
@@ -353,7 +353,7 @@ BOOST_AUTO_TEST_CASE(test_IsStandard)
 
     // 41-byte TX_NULL_DATA (non-standard)
     t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef3800");
-    BOOST_CHECK(!IsStandardTx(t, reason));
+    BOOST_CHECK(IsStandardTx(t, reason));
 
     // TX_NULL_DATA w/o PUSHDATA
     t.vout.resize(1);
