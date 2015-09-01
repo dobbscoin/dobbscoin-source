cadcfdc3c6f10d38c7e95d60eb319ae1788f752d
Earlz <earlz@earlz.net>
2015-08-31 23:17:18 -0400

    add fork to correct coin supply bug

--- src/main.cpp
+++ src/main.cpp
@@ -1291,6 +1291,10 @@ CAmount GetBlockValue(int nHeight, const CAmount& nFees)
     {
         nSubsidy=10*COIN;
     }
+    if(!Params().AllowMinDifficultyBlocks() && nHeight>=69791)
+    {
+        nSubsidy=10*COIN;
+    }
     // Subsidy is cut in half every 210000 blocks, which will occur approximately every 4 years
     nSubsidy >>= (nHeight / 210000);
 
