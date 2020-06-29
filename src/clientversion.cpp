005c1304192dd62b86592eea424405572c964e72
trumpliedpeopledied <trumpliedpeopledied@protonmail.com>
2020-06-29 09:52:52 -0600

    Change Halving and Rewards, version bump.

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both dobbscoind and dobbscoin-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("EndofTimes");
 
 /**
  * Client version number
--- src/version.h
+++ src/version.h
@@ -9,7 +9,8 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70003;
+//!Increase so that older versions will be cut off after the fork. 
+static const int PROTOCOL_VERSION = 70005;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -18,7 +19,9 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
+//! interim disconnect build is unnumbered for but for all intents and 
+//! purposes its meta-build 70004
+static const int MIN_PEER_PROTO_VERSION = 70003;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
