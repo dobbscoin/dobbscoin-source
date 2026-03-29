8c379e45a530c889b445692291a61d0f1b7136a2
EndCiv <endciv@ns1.wholesalehosting.net>
2026-03-29 22:07:55 +0000

    WIP: Windows depends build failing at Qt spec_pre.prf on Ubuntu 24.04

--- depends/packages/qrencode.mk
+++ depends/packages/qrencode.mk
@@ -1,11 +1,18 @@
 package=qrencode
+
 $(package)_version=3.4.3
-$(package)_download_path=http://snapshot.debian.org/archive/debian/20141007T040000Z/pool/main/q/qrencode
-$(package)_file_name=qrencode-$(qrencode_version).tar.bz2
+
+# We already provide the file locally in depends/sources
+$(package)_download_path=
+
+# Correct filename pattern
+$(package)_file_name=qrencode-$($(package)_version).tar.bz2
+
+# Correct checksum for your downloaded file
 $(package)_sha256_hash=dfd71487513c871bad485806bfd1fdb304dedc84d2b01a8fb8e0940b50597a98
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared -without-tools --disable-sdltest
+$(package)_config_opts=--disable-shared --without-tools --disable-sdltest
 $(package)_config_opts_linux=--with-pic
 endef
 
