2b1fd22b9bc948508db33c8da10fe2f1e80ec8b6
guru autodeploy <autodeploy@guru.subgenius.net>
2026-09-23 17:36:32 +0000

    depends: miniupnpc 1.9.20140701 -> 2.3.3 (#43)
    2.3.3 is API version 21. src/net.cpp already chooses the API>=14
    upnpDiscover() and API>=18 UPNP_GetValidIGD() signatures on
    MINIUPNPC_API_VERSION, so no source change is needed; configure still adds
    -DMINIUPNP_STATICLIB / -DNATPMP_STATICLIB for the static link (#45).
    
    2.x moved headers to include/ and the Unix library to build/. On mingw,
    Makefile.mingw's _WIN32_WINNT=0x501 (XP) is replaced with 0x0601; its %.o
    rule compiles the library objects with -DMINIUPNP_STATICLIB itself.
    
    Upstream: https://miniupnp.tuxfamily.org/files/miniupnpc-2.3.3.tar.gz
    sha256 d52a0afa...
    
    Co-Authored-By: MWOWM <mwowm@subgenius.vip>

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -1,28 +1,28 @@
 package=miniupnpc
-$(package)_version=1.9.20140701
-$(package)_download_path=http://miniupnp.free.fr/files
+# 2.x is API version 21. src/net.cpp already selects the matching
+# upnpDiscover()/UPNP_GetValidIGD() signatures on MINIUPNPC_API_VERSION.
+$(package)_version=2.3.3
+$(package)_download_path=https://miniupnp.tuxfamily.org/files
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=26f3985bad7768b8483b793448ae49414cdc4451d0ec83e7c1944367e15f9f07
+$(package)_sha256_hash=d52a0afa614ad6c088cc9ddff1ae7d29c8c595ac5fdd321170a05f41e634bd1a
 
 define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
-$(package)_build_opts_darwin=OS=Darwin
-$(package)_build_opts_mingw32=-f Makefile.mingw
+$(package)_build_opts_darwin=LIBTOOL="$($(package)_libtool)"
+# Makefile.mingw appends -D_WIN32_WINNT=0x501 (XP) to CPPFLAGS; overriding
+# CPPFLAGS on the command line replaces that with Windows 7 (0x0601), which is
+# what the rest of the build targets. The library objects themselves are
+# compiled with -DMINIUPNP_STATICLIB by Makefile.mingw's %.o rule.
+$(package)_build_opts_mingw32=-f Makefile.mingw CPPFLAGS="-DNDEBUG -D_WIN32_WINNT=0x0601 -Iinclude -I. $($(package)_cppflags)"
 $(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
 endef
 
-define $(package)_preprocess_cmds
-  mkdir dll && \
-  sed -e 's|MINIUPNPC_VERSION_STRING \"version\"|MINIUPNPC_VERSION_STRING \"$($(package)_version)\"|' -e 's|OS/version|$(host)|' miniupnpcstrings.h.in > miniupnpcstrings.h && \
-  sed -i.old "s|miniupnpcstrings.h: miniupnpcstrings.h.in wingenminiupnpcstrings|miniupnpcstrings.h: miniupnpcstrings.h.in|" Makefile.mingw
-endef
-
 define $(package)_build_cmds
-	$(MAKE) libminiupnpc.a $($(package)_build_opts)
+	$(MAKE) $(if $(findstring mingw32,$(host_os)),libminiupnpc.a,build/libminiupnpc.a) $($(package)_build_opts)
 endef
 
 define $(package)_stage_cmds
 	mkdir -p $($(package)_staging_prefix_dir)/include/miniupnpc $($(package)_staging_prefix_dir)/lib &&\
-	install *.h $($(package)_staging_prefix_dir)/include/miniupnpc &&\
-	install libminiupnpc.a $($(package)_staging_prefix_dir)/lib
+	install include/*.h $($(package)_staging_prefix_dir)/include/miniupnpc &&\
+	install `test -f build/libminiupnpc.a && echo build/libminiupnpc.a || echo libminiupnpc.a` $($(package)_staging_prefix_dir)/lib
 endef
